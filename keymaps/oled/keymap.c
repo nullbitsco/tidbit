6d35219c6904e1c526cce941c8f00649b0d19fa2
Aashrey Sharma <aashrey9sharma@gmail.com>
2021-07-12 13:02:37 -0400

    Fixed encoder_update return type mismatch in OLED keycap

--- keymaps/oled/keymap.c
+++ keymaps/oled/keymap.c
@@ -129,12 +129,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (clockwise) {
     tap_code(KC_VOLU);
   } else {
     tap_code(KC_VOLD);
-  }  
+  } 
+  return true;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
