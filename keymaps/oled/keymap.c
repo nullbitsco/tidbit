1b18c5f087bfaa3c425aa298bb4b3a4fc85a26a3
Jay Greco <jayv.greco@gmail.com>
2021-08-25 02:56:51 +0000

    Fix 0 key for 2U builds

--- keymaps/oled/keymap.c
+++ keymaps/oled/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_KP_7, KC_KP_8,  KC_KP_9,        KC_KP_PLUS, \
   KC_KP_4, KC_KP_5,  KC_KP_6,        KC_NO, \
   KC_KP_1, KC_KP_2,  KC_KP_3,        TD(TD_ENTER_LAYER), \
-  KC_NO,   KC_KP_0,  KC_KP_DOT,      KC_NO \
+  KC_KP_0, KC_KP_0,  KC_KP_DOT,      KC_NO \
   ),
   // Function layer (numpad)
   [_FUNC] = LAYOUT(
